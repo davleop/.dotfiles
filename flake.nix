c681670918e232fd16bfbd23a6f9591f79695abf
davleop <davleop@github.com>
2025-08-28 21:29:20 -0400

    chore: update discord

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
-
   description = "One flake to rule them all";
 
   inputs = {
@@ -9,20 +8,50 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
-    {
-      nixosConfigurations = {
-        tungsten = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./configuration.nix ];
+  let
+    discordOverlay = (final: prev: {
+      discord = prev.discord.overrideAttrs (old: rec {
+        version = "0.0.104";
+        src = prev.fetchurl {
+          url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+          sha256 = "19rx408b5zwdishli5pdgd2as0nb91nr3shwa0j3hdfih7sh43z3";
         };
-      };
-
-      homeConfigurations = {
-        david = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages."x86_64-linux";
-          modules = [ ./home.nix ];
-        };
+      });
+    });
+  in
+  {
+    nixosConfigurations = {
+      tungsten = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          # Make the overlay and unfree active for the system build
+          ({ ... }: {
+            nixpkgs = {
+              overlays = [ discordOverlay ];
+              config.allowUnfree = true;
+            };
+          })
+        ];
       };
     };
 
+    homeConfigurations = {
+      david = home-manager.lib.homeManagerConfiguration {
+        # keep your existing pkgs line if you want,
+        # but ensure HM sees the overlay too:
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        modules = [
+          ./home.nix
+          ({ ... }: {
+            nixpkgs = {
+              overlays = [ discordOverlay ];
+              config.allowUnfree = true;
+            };
+          })
+        ];
+      };
+    };
+  };
 }
+

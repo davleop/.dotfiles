2a73297d0a941f1adad095b485a8dd65c58b6bba
davleop <davleop@github.com>
2026-03-25 23:01:05 -0400

    add spell check

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,13 @@
 require('config.options')
 require('config.keybinds')
 require('config.lazy')
+
+
+-- general settings ...
+vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
+  pattern = {"*.md","*.markdown","*.txt","*.typ"},
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+  end,
+})
